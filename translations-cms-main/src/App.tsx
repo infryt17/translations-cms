33a5ea0d96bf3f77ff7dbde9e99233e9295b5f41
Frank Chlebda <frankchlebda@gmail.com>
2025-12-12 15:02:31 +0100

    router and layout updaet

--- translations-cms-main/src/App.tsx
+++ translations-cms-main/src/App.tsx
@@ -1,5 +1,13 @@
 import './main.scss';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
+import Analytics from './pages/analitics';
+import Languages from './pages/Languages';
 
 function App() {
   return (
@@ -14,18 +22,44 @@ function App() {
           <h2>Menu</h2>
 
           <div className="userCard">User</div>
-
-          <a href="#">Overview</a>
-          <a href="#">Analytics</a>
-          <a href="#">Languages</a>
+          <Switch>
+            <Routes>
+              <Route path="/" />
+              <Route path="/analytics" element={<Analytics />} />
+              <Route path="/languages" element={<Languages />} />
+            </Routes>
+          </Switch>
         </nav>
 
         <main>
           <div className="grid">
             <div className="cardMain">Main Card</div>
-            <div className="card">Small Card</div>
-            <div className="card">Small Card</div>
+            <div className="card">
+              <h2>Small Card</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+                placeat dolorem iste qui, similique consectetur optio sequi amet
+                expedita, har um eum esse cum voluptas sint soluta nobis
+                recusandae obcaecati? Quia. expedita, har um eum esse cum
+                voluptas sint soluta nobis recusandae obcaecati? Quia. placeat
+                dolorem iste qui, similique consectetur optio sequi amet
+                expedita, har um eum esse cum voluptas sint soluta nobis
+              </p>
+            </div>
+            <div className="card">
+              <h2>Small Card</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
+                placeat dolorem iste qui, similique consectetur optio sequi amet
+                expedita, har um eum esse cum voluptas sint soluta nobis
+                recusandae obcaecati? Quia. expedita, har um eum esse cum
+                voluptas sint soluta nobis recusandae obcaecati? Quia. placeat
+                dolorem iste qui, similique consectetur optio sequi amet
+                expedita, har um eum esse cum voluptas sint soluta nobis
+              </p>
+            </div>
             <div className="cardWide">Wide Card</div>
+            <div className="outputCard">Log/error output</div>
           </div>
         </main>
       </div>
