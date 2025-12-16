f1b8ee81ccbcc4cfb8a318efda30b3dd6e425cf2
Frank Chlebda <frankchlebda@gmail.com>
2025-12-16 15:48:49 +0100

    base for component

---
+++ src/components/LangList.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export default function LangList() {
+  const [languages, setLanguages] = useState(['English, Spanish']);
+  const [newLang, setNewLang] = useState('');
+
+  function addLang() {}
+
+  function stopLang() {}
+
+  function resetTranslating() {}
+}
