edcf710fcbcc3e11b2cd0d846832ea57953d22e3
Frank Chlebda <frankchlebda@gmail.com>
2025-12-16 12:25:28 +0100

    layout changes

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,20 +1,33 @@
+import '../main.scss';
 export default function Analytics() {
   return (
     <>
-      <h1>Analytics page</h1>
-      <div>
-        <div>
-          <h3>Languages settings</h3>
-          <div>
-            <p>block or unblock laungage</p>
+      <h1 className="topbar">Analytics</h1>
+
+      <div className="layout">
+        <section className="grid">
+          <div className="card cardMain">
+            <h3>Last changes log</h3>
+            <ul>
+              <li>Updated users </li>
+              <li> lang support</li>
+              <li>Dashboard </li>
+            </ul>
           </div>
-          <div>
-            <p>translating errors</p>
+
+          <div className="card outputCard">
+            <h3>Users:</h3>
+            <p>2000</p>
+            <span>+""% from last month</span>
           </div>
-        </div>
-        <div>
-          <h3>Last changes log</h3>
-        </div>
+
+          <div className="card cardWide">
+            <h3>Most used languages: </h3>
+            <div className="langCont">
+              <div className="activeLng"></div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
--- src/pages/Languages.tsx
+++ src/pages/Languages.tsx
@@ -1,14 +1,22 @@
 export default function Languages() {
   return (
     <>
-      <h1>Languages page</h1>
+      <h1 className="topbar">Languages page</h1>
       <div className="languagesCont">
         <div className="activeLng">
           <h3>Active Languages</h3>
         </div>
+
         <div className="inWorkLng">
           <h3>Languages in work</h3>
         </div>
+      </div>
+      <h3>Languages settings</h3>
+      <div>
+        <p>block or unblock laungage</p>
+      </div>
+      <div>
+        <p>translating errors</p>
       </div>
     </>
   );
