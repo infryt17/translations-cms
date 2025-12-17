878ff18f086509813ea36c65e1532aec4bfa0a4b
Frank Chlebda <frankchlebda@gmail.com>
2025-12-17 14:14:16 +0100

    adding context to languages component

---
+++ src/context/langContext.tsx
@@ -0,0 +1,30 @@
+import React, {
+  useContext,
+  createContext,
+  useState,
+  type ReactNode,
+} from 'react';
+
+interface LanguageContextValue {
+  activeLanguages: string[];
+  setActiveLanguages: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const LanguageContext = createContext<LanguageContextValue | undefined>(
+  undefined,
+);
+interface LanguageProviderProps {
+  children: ReactNode;
+}
+export function LanguageProvider({ children }: LanguageProviderProps) {
+  const [activeLanguages, setActiveLanguages] = useState<string[]>([
+    'English',
+    'Spanish',
+  ]);
+  return (
+    <LanguageContext.Provider value={{ activeLanguages, setActiveLanguages }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+export function
